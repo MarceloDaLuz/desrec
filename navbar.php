c62afd59e8b6f7f157c6f7f7db15cf5601d1ba8c
Marcelo Muniz da Luz <ma_celinho18@hotmail.com>
2018-03-28 20:02:14 -0300

    conteudo adicionado na pag colete
    css. colete.

--- navbar.php
+++ navbar.php
@@ -11,10 +11,10 @@
                 <ul class="navbar-nav">
                     <!-- Abas-->
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Inicio</a>
+                        <a class="nav-link" href="./index.php">Inicio</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Colete</a>
+                        <a class="nav-link" href="./colete.php">Colete</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="#">Descarte</a>
