4652b8941221942eb82fd30091a7e814a19f26fb
MarceloDaLuz <ma_celinho18@hotmail.com>
2018-04-03 10:02:10 -0300

    only CI
    remove m-v-c folders

--- V/card.php
+++
@@ -1,9 +0,0 @@
-<div class="card dr-card">
-            <div class="card-body">
-                <h5 class="card-title">Material</h5>
-                <hr>
-                <h6 class="card-subtitle mb-2 text-muted"><i>Doador</i></h6>
-                <p class="card-text dr-content-card">Descrição sobre o material!</p>    
-                <a href="#" class="btn btn-link card-link dr-btn-card">Acessar card(Detalhes)</a>
-            </div>
-            </div>
